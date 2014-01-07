8b15697c1ee5c6439bd6fc44eb782096ee80470b
David Cramblett <dcramble@mesd.k12.or.us>
2014-01-06 17:03:13 -0800

    Add new input control type

---
+++ src/JasperClient/Client/InputControlMultiSelectCheckbox.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * @author David Cramblett
+ */
+
+namespace JasperClient\Client;
+
+class InputControlMultiSelectCheckbox extends AbstractInputControl {
+
+    private $optionList;
+
+    function __construct($id, $label, $mandatory, $readOnly, $type, $uri, $visible, $state) {
+
+        parent::__construct($id, $label, $mandatory, $readOnly, $type, $uri, $visible, $state);
+
+        if ("Custom" == APP_REPORT_INPUT_CONTROL) {
+            $customInputClass = "JasperClient\Custom\InputControlOptionList".ucfirst($this->getId());
+            $optionListClass = new $customInputClass($state);
+            $this->optionList = $optionListClass->getList();
+        }
+        elseif ("Fallback" == APP_REPORT_INPUT_CONTROL) {
+            $customInputClass = "JasperClient\Custom\InputControlOptionList".ucfirst($this->getId());
+            if (true == class_exists($customInputClass)) {
+                $optionListClass = new $customInputClass($state);
+                $this->optionList = $optionListClass->getList();
+            }
+            else {
+                 $inputControlStateArray = JasperHelper::convertInputControlState($state);
+
+                foreach ($inputControlStateArray["option"] as $key => $option) {
+
+                    $this->optionList[] = new InputControlOption (
+                        $option["value"],
+                        $option["label"],
+                        $option["selected"]
+                    );
+                }
+            }
+        }
+        elseif ("Jasper" == APP_REPORT_INPUT_CONTROL) {
+
+            $inputControlStateArray = JasperHelper::convertInputControlState($state);
+
+            foreach ($inputControlStateArray["option"] as $key => $option) {
+
+                $this->optionList[] = new InputControlOption (
+                    $option["value"],
+                    $option["label"],
+                    $option["selected"]
+                );
+            }
+        }
+    }
+
+
+    public function getOptionList() {
+        return $this->optionList;
+    }
+
+}
